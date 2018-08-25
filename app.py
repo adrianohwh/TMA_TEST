f8e57f3c40ea1fb9db2c35e55dac33950dc9a4d5
adrianohwh <tB498311>
2018-08-25 16:40:17 +0800

    aaaa

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.write("WELCOME TO MY PAAS WEB APP\n")
-		self.response.write("Name: Adrian Oh Wei Hao\n")
-		self.response.write("SUSS ID: E1711467\n")
-		self.response.write("Course Code: ICT335")
+		self.response.write("WELCOME TO MY PAAS WEB APP""\n")
+		self.response.write("Name: Adrian Oh Wei Hao""\n")
+		self.response.write("SUSS ID: E1711467""\n")
+		self.response.write("Course Code: ICT335""\n")
 
 app = webapp2.WSGIApplication([('/', MainPage),
 								],
